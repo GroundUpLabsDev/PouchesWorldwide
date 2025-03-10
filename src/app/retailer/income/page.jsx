434d8b59ed17907db6302453be93e60d3dcced35
VIHANGA <vihanga2003nimsara@gmail.com>
2025-03-11 04:06:02 +0530

    not selecting a tab by default fix

--- src/app/retailer/income/page.jsx
+++ src/app/retailer/income/page.jsx
@@ -10,7 +10,7 @@ import Footer from "@/components/Footer";
 
 
 export default function InventoryTabs() {
-  const [activeTab, setActiveTab] = useState("stock");
+  const [activeTab, setActiveTab] = useState("earnings");
 
   return (
     <>
