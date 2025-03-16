ba42eb58f688a684c227a3d08149eebf2963554f
VIHANGA <vihanga2003nimsara@gmail.com>
2025-03-16 21:50:33 +0530

    changed app page and layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,20 +1,19 @@
-
-
-import '../styles/globals.css';
-import { Poppins, Source_Serif_4, Inter } from 'next/font/google';
+import Header from "@/components/Header";
+import "../styles/globals.css";
+import { Poppins, Source_Serif_4, Inter } from "next/font/google";
 
 // Configure Poppins
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'], // Include the font weights you need
-  variable: '--font-poppins', // Define a CSS variable for Poppins
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"], // Include the font weights you need
+  variable: "--font-poppins", // Define a CSS variable for Poppins
 });
 
 // Configure Source Serif 4
 const sourceSerif = Source_Serif_4({
-  subsets: ['latin'],
-  weight: ['400', '600', '700'], // Include the font weights you need
-  variable: '--font-source-serif', // Define a CSS variable for Source Serif 4
+  subsets: ["latin"],
+  weight: ["400", "600", "700"], // Include the font weights you need
+  variable: "--font-source-serif", // Define a CSS variable for Source Serif 4
 });
 
 const inter = Inter({
@@ -26,7 +25,10 @@ const inter = Inter({
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${poppins.variable} ${sourceSerif.variable}`}>
-      <body className="font-poppins">{children}</body>
+      <body className="font-poppins">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,18 @@
 
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation"; // Import useRouter for redirection
-import Image from 'next/image';
+import Image from "next/image";
 import Link from "next/link";
-import Header from "@/components/Header";
-import PrimaryList from '@/components/Primarylist';
-import ProductCard from '@/components/ProductCard';
-import Feedback from '@/components/Feedback';
-import FAQ from '@/components/FAQ';
-import Footer from '../components/Footer';
-import Banner from '../components/Banner';
-import BlackBanner from '@/components/BlackBanner';
-import Pagination from '@/components/Pagination';
+import PrimaryList from "@/components/Primarylist";
+import ProductCard from "@/components/ProductCard";
+import Feedback from "@/components/Feedback";
+import FAQ from "@/components/FAQ";
+import Footer from "../components/Footer";
+import Banner from "../components/Banner";
+import BlackBanner from "@/components/BlackBanner";
+import Pagination from "@/components/Pagination";
 import { fetchProducts } from "@/app/utils/fetchProducts"; // Import the fetchProducts function
-import Preloader from "@/components/Preloader";  // Import Preloader
+import Preloader from "@/components/Preloader"; // Import Preloader
 import { getUserRole } from "@/app/utils/getUserRole"; // Import getUserRole function
 
 export default function Home() {
@@ -45,120 +44,111 @@ export default function Home() {
     }
   }, [router]); // Make sure to run this only when the router is ready
 
-
   if (loading) {
     return <Preloader />; // Show preloader while loading
   }
 
-
   return (
-    
     <div className="relative">
-        <Header />
-
-
-  <Link href="/">
-    {/* Hero section */}
-    <div className="relative w-full px-0 md:px-6 lg:px-9 bg-black pt-8 overflow-hidden box-border pb-8">
+      <Link href="/">
+        {/* Hero section */}
+        <div className="relative w-full px-0 md:px-6 lg:px-9 bg-black pt-8 overflow-hidden box-border pb-8">
           {/* Image */}
           <Image
             src="https://pouchesworldwide.com/strapi/uploads/puxx_banner_50583c11de.jpg"
             alt="Banner Image"
             width={1240}
-            height={423} 
+            height={423}
             priority
             className="object-cover max-w-screen-2xl mx-auto w-full h-auto max-h-[400px] sm:max-h-[500px] md:max-h-[600px] lg:max-h-[700px] rounded-lg"
             draggable={false}
             onContextMenu={(e) => e.preventDefault()}
           />
-    </div>
-  </Link>
+        </div>
+      </Link>
 
-  {/* Logo bar */}
-  <Banner />
+      {/* Logo bar */}
+      <Banner />
 
-  {/* Center PrimaryList */}
-  <div className="flex justify-center items-center py-8">
-    <PrimaryList />
-  </div>
+      {/* Center PrimaryList */}
+      <div className="flex justify-center items-center py-8">
+        <PrimaryList />
+      </div>
 
-  {/* Main banner 
+      {/* Main banner 
   <div><BlackBanner /></div>*/}
 
- {/* Split section */}
-<div className="flex flex-col md:flex-row-reverse p-4 md:p-6 lg:p-4 justify-center items-center mt-[50px] mb-8 mx-auto max-w-[1220px]">
-
-{/* Product Cards - Left Side */}
-<div className="w-full md:w-1/2 flex justify-center">
-  <div className="grid grid-cols-1 sm:grid-cols-2 gap-[30px] justify-items-center w-full">
-    {!loading && products
-      .filter((product) => product.Fruit2 === true)
-      .slice(0, 2).map((product) => (
-        <div key={product.id} className="w-full sm:w-auto flex justify-center">
-          <ProductCard product={product} />
+      {/* Split section */}
+      <div className="flex flex-col md:flex-row-reverse p-4 md:p-6 lg:p-4 justify-center items-center mt-[50px] mb-8 mx-auto max-w-[1220px]">
+        {/* Product Cards - Left Side */}
+        <div className="w-full md:w-1/2 flex justify-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-[30px] justify-items-center w-full">
+            {!loading &&
+              products
+                .filter((product) => product.Fruit2 === true)
+                .slice(0, 2)
+                .map((product) => (
+                  <div key={product.id} className="w-full sm:w-auto flex justify-center">
+                    <ProductCard product={product} />
+                  </div>
+                ))}
+          </div>
         </div>
-      ))}
-  </div>
-</div>
 
-{/* Rect Banner - Right Side */}
-<Link href="/" className="w-full md:w-auto">
-  <div className="h-[250px] sm:h-[350px] md:h-[452px] flex-shrink-0 flex justify-center items-center w-full md:w-auto mb-4 md:mb-0 md:mr-[50px] lg:mr-[95px]">
-    <Image
-      src="https://pouchesworldwide.com/strapi/uploads/rectban_301a0242af.jpg"
-      alt="Banner Image"
-      width={0}
-      height={0}
-      sizes="100vw"
-      priority
-      className="object-cover w-full h-full max-w-[400px] md:max-w-none rounded-lg"
-      draggable={false}
-      onContextMenu={(e) => e.preventDefault()}
-    />
-  </div>
-</Link>
+        {/* Rect Banner - Right Side */}
+        <Link href="/" className="w-full md:w-auto">
+          <div className="h-[250px] sm:h-[350px] md:h-[452px] flex-shrink-0 flex justify-center items-center w-full md:w-auto mb-4 md:mb-0 md:mr-[50px] lg:mr-[95px]">
+            <Image
+              src="https://pouchesworldwide.com/strapi/uploads/rectban_301a0242af.jpg"
+              alt="Banner Image"
+              width={0}
+              height={0}
+              sizes="100vw"
+              priority
+              className="object-cover w-full h-full max-w-[400px] md:max-w-none rounded-lg"
+              draggable={false}
+              onContextMenu={(e) => e.preventDefault()}
+            />
+          </div>
+        </Link>
+      </div>
 
-</div>
+      {/* Products grid */}
+      <div className="relative">
+        <div className="absolute inset-0 h-[290px] bg-black z-0 mt-[400px] hidden sm:block"></div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 z-10 relative">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 justify-items-center">
+            {!loading &&
+              products
+                .filter((product) => product.Fruit === true)
+                .slice(0, 4)
+                .map(
+                  (
+                    product // Display only the first 6 products
+                  ) => (
+                    <div key={product.id} className="w-full max-w-sm flex justify-center">
+                      <ProductCard product={product} />
+                    </div>
+                  )
+                )}
+          </div>
 
-
-
-
-  
-
-  {/* Products grid */}
-  <div className="relative">
-  <div className="absolute inset-0 h-[290px] bg-black z-0 mt-[400px] hidden sm:block"></div>
-  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 z-10 relative">
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 sm:gap-6 justify-items-center">
-      {!loading && products
-      .filter((product) => product.Fruit === true)
-      .slice(0, 4).map((product) => (  // Display only the first 6 products
-        <div key={product.id} className="w-full max-w-sm flex justify-center">
-          <ProductCard product={product} />
-        </div> 
-      ))}
-    </div>
-
-    {/* Pagination 
+          {/* Pagination 
     <div className="flex justify-center">
       <Pagination totalPages={10} />
     </div>*/}
-  </div> 
-</div>
+        </div>
+      </div>
 
+      <div className="bg-black">
+        <Feedback />
+      </div>
 
-  <div className='bg-black'>
-    <Feedback />
-  </div>
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 min-h-[200px]">
+        <FAQ />
+      </div>
 
-  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 min-h-[200px]">
-  <FAQ />
-</div>
-
-
-  <Footer />
-</div>
-
+      <Footer />
+    </div>
   );
 }
- 
